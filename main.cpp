b0ddb76f05481c46a66b24a2a112af6483f4c258
Me <rare2oooo1@gmail.com>
2015-04-06 09:16:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
-#include <SFML/graphics.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
 using namespace sf;
 
 int main()
 {
-
+	int LogoY = 300;
+	int LogoX = 5;
+	int PlayY = 0;
+	int PlayX = 0;
+	RenderWindow window(sf::VideoMode(1920, 1080), "PvP Network");
+	Font Mfont;
+	Mfont.loadFromFile("28 Days Later.ttf");
+	Text logo("PvP Network", Mfont);
+	logo.setCharacterSize(150);
+	logo.setStyle(sf::Text::Bold);
+	logo.setColor(sf::Color::Red);
+	logo.setPosition (LogoY, LogoX);
+	Text play("PLAY", Mfont);
+	play.setCharacterSize(100);
+	play.setStyle(Text::Bold);
+	play.setColor(Color::Red);
+	play.setPosition (PlayY, PlayX);
+	          
+	  Music themeSong;
+    if (!themeSong.openFromFile("2.2.wav"))
+        return EXIT_FAILURE;
+	while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+		}
+	    	window.clear();
+        window.draw(logo);
+		    window.draw(play);
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
